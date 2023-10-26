f918f191e9901cb65234b326958391da6aff55c6
Haroon Raza <146657522+lonengineer@users.noreply.github.com>
2023-10-26 18:00:03 +0500

    Update freq_spec.m
    x axis limit added

--- freq_spec.m
+++ freq_spec.m
@@ -1,9 +1,10 @@
-function [f w] = freq_spec(signal,sampling_rate)
+function [f w] = freq_spec(signal,sampling_rate,axis_limit)
 %FREQ_SPEC plots frequency spectrum of a provided signal
     % FREQ_SPEC(signal,samping_rate),   signal is the signal whose spectrum is to be plotted and sampling_rate is number of samples taken
     % This function fails if the sampling rate is low and frequency is very high (comparitively)
     % This function plots spectrum with respect to w (rad/s) and not f
     % This function return vectors [f,w] to plot frequency spectrum of a signal
+    % axis_limit limits the x axis to axis_limit from -axis_limit
 
 f = abs(fftshift(fft(signal)/length(signal)));
 w = 2*pi.*(linspace(-sampling_rate/2, sampling_rate/2,length(f)));
@@ -12,5 +13,7 @@ if(nargout==0)
     title('Frequency Spectrum');
     ylabel('M(\omega)','FontWeight','bold')
     xlabel('\omega','FontWeight','bold')
+    if nargin >= 3
+        xlim([-axis_limit axis_limit]);
 end
 end
