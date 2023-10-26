91694de8cbb38d7054cd47cc98a49728a62eafee
Haroon Raza <146657522+lonengineer@users.noreply.github.com>
2023-10-26 16:43:14 +0500

    Add files via upload

---
+++ freq_spec.m
@@ -0,0 +1,16 @@
+function [f w] = freq_spec(signal, sampling_rate)
+%FREQ_SPEC plots frequency spectrum of a provided signal
+    % FREQ_SPEC(signal,samping_rate),   signal is the signal whose spectrum is to be plotted and sampling_rate is number of samples taken
+    % This function fails if the sampling rate is low and frequency is very high (comparitively)
+    % This function plots spectrum with respect to w (rad/s) and not f
+    % This function return vectors [f,w] to plot frequency spectrum of a signal
+
+f = abs(fftshift(fft(signal)/length(signal)));
+w = 2*pi.*(linspace(-sampling_rate/2, sampling_rate/2,length(f)));
+if(nargout==0)
+    plot(w,f,LineWidth=2)
+    title('Frequency Spectrum');
+    ylabel('M(\omega)','FontWeight','bold')
+    xlabel('\omega','FontWeight','bold')
+end
+end
