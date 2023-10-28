45f65b77f9d86c515491dcc0cda3f19a1a6fff11
Haroon Raza <146657522+lonengineer@users.noreply.github.com>
2023-10-28 12:33:39 +0500

    Add files via upload

--- freq_spec.m
+++ freq_spec.m
@@ -1,10 +1,12 @@
 function [f w] = freq_spec(signal,sampling_rate,axis_limit)
 %FREQ_SPEC plots frequency spectrum of a provided signal
-    % FREQ_SPEC(signal,samping_rate),   signal is the signal whose spectrum is to be plotted and sampling_rate is number of samples taken
-    % This function fails if the sampling rate is low and frequency is very high (comparitively)
-    % This function plots spectrum with respect to w (rad/s) and not f
-    % This function return vectors [f,w] to plot frequency spectrum of a signal
-    % axis_limit limits the x axis to axis_limit from -axis_limit
+% FREQ_SPEC(signal,samping_rate),   signal is the signal whose spectrum is to 
+% be plotted and sampling_rate is number of samples taken
+% sampling rate must be almost 5 to 10 times the frequency for accurate
+% x-axis
+% This function plots spectrum with respect to w (rad/s) and not f
+% This function return vectors [f,w] to plot frequency spectrum of a signal
+% axis_limit limits the x axis to axis_limit from -axis_limit
 
 f = abs(fftshift(fft(signal)/length(signal)));
 w = 2*pi.*(linspace(-sampling_rate/2, sampling_rate/2,length(f)));
