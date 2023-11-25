cf6b8ac9482123cc9d3ab4b6d16a5c590423e2a9
Haroon Raza <146657522+voltvirtuoso@users.noreply.github.com>
2023-11-25 11:22:16 +0500

    Update rootlocus.m

--- rootlocus.m
+++ rootlocus.m
@@ -1,22 +1,57 @@
 function rootlocus(G)
-% rlocus(G)
-% fig = gcf;
-% x_lim=fig.CurrentAxes.XLim;
-% y_lim=fig.CurrentAxes.YLim;
-% close
-[~,k]=rlocus(G);
+ % ROOTLOCUS  Evans root locus.
+ % 
+ %    rootlocus(SYS) computes and plots the root locus of the single-input,
+ %    single-output LTI model SYS and visulasises how poles moves at different
+ %    values of K. The root locus plot is used to analyze the negative feedback loop
+ % 
+ %                      +-----+
+ %          ---->O----->| SYS |----+---->
+ %              -|      +-----+    |
+ %               |                 |
+ %               |       +---+     |
+ %               +-------| K |<----+
+ %                       +---+
+ % 
+ %    and shows the trajectories of the closed-loop poles when the feedback 
+ %    gain K varies from 0 to Inf.  rootlocus automatically generates a set of 
+ %    positive gain values that produce a smooth plot.  
+
+ 
+if ni==0,
+    if no~=0,  error('Missing input argument(s).'),  end
+    eval('exresp(''rlocus'');')
+    return
+end
+[z,k]=rlocus(G);
 label("Root Locus","Real Axis (seconds^{-1})","Imaginary Axis (seconds^{-1})")
-num = asymtotes(G);
-% xlim(x_lim)
-% ylim(y_lim)
 hold on
+num = asymtotes(G);
+z=z(:,1:length(z)-(num+1));
+z=z(:);
+x_l = min(real(z));
+x_h = max(real(z));
+y_l = min(imag(z)); y_h = max(imag(z));
+
+if (x_h == 0 && x_l == 0)
+    x_h = 5; x_l = -5;
+end
+if (y_h == 0 && y_l == 0)
+    y_h = 5; y_l = -5;
+end
+
+x_lim = [x_l+(x_l*0.05) x_h+(x_h*0.05)];
+y_lim = [y_l+(y_l*0.05) y_h+(y_h*0.05)];
+asymtotes(G);
+xlim(x_lim)
+ylim(y_lim)
 for i = 1:length(k)-num
      if k(i)~=0 & k(i)~=Inf 
         sys = feedback(series(tf(k(i)),G),tf(1));
         [~, den] = tfdata(sys);
         r = roots(den{1});
         hold on
-        plot(real(r),imag(r),'rx',LineWidth=2)
+        plot(real(r),imag(r),'rx',LineWidth=1)
         pause(1/50)
      end
 end
@@ -36,11 +71,13 @@ for i = 1:(length(p)-length(z))
     temp=phi(i);
     theta = [theta temp];
 end
-angleplot(theta,real(c),imag(c))
+if nargout==0
+    angleplot(theta,real(c),imag(c))
+end
 end
 
 function angleplot(angle,x,y)
-lineLength2 = 30;
+lineLength2 = 65535;
 hold on;
 for i = 1:numel(angle)
   e = x + lineLength2*cosd(angle(i));
